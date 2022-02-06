39d7871b73b41e9fa325e0cd0492f50f62e741d9
kubistmi <kubistmi@gmail.com>
2022-02-06 18:58:36 +0100

    W5 - preclass changes

--- w5/seminar5.R
+++ w5/seminar5.R
@@ -1,7 +1,7 @@
 #####################
 ## Seminar 5       ##
 ## Michal Kubista  ##
-## 5 February 2021 ##
+## 7 February 2022 ##
 #####################
 
 # install.packages("rpart")
@@ -17,73 +17,99 @@ sapply(
 
 #--- 1.1 ETL -------------------------------------------------------------------
 if (!dir.exists("w5/data")) {
-dir.create("w5/data")
+    dir.create("w5/data")
 }
-# data from https://community.tableau.com/docs/DOC-1236
-# doesn't work anymore :(
-# url = "https://community.tableau.com/servlet/JiveServlet/downloadBody/1236-102-2-15278/Sample%20-%20Superstore.xls"
-# download.file(url, "w5/data/transac.xls", mode = "wb")
-# rm(url)
-# download from https://drive.google.com/drive/folders/1M1nsc282Eoi_ax_O6piuDrWjYcxwAZWl?usp=sharing
 
-raw = read_excel("w5/data/transac.xls") %>% as.data.table()
+# download from https://drive.google.com/drive/folders/1M1nsc282Eoi_ax_O6piuDrWjYcxwAZWl?usp=sharing
 ## ID coercing
+raw = read_excel("w5/data/transac.xls") %>% as_tibble()
 
 str(raw)
 summary(raw)
 
 map_dbl(raw, ~length(unique(.)))
 
+colnm =
+    raw %>%
+    colnames() %>% 
+    gsub(" |-","_",.) %>% 
+    tolower()
+
+length(unique(colnm)) == ncol(raw)
+colnames(raw) = colnm
+
 #---- 1.1.1 TABLES SPLITTING ---------------------------------------------------
 ## customer table
-cust = raw[,.(cust_name = unique(`Customer Name`),
-               segment = unique(Segment)
-               ),
-            by = "Customer ID"]
-setnames(cust, "Customer ID", "cust_ID")
+cust =
+    raw %>% 
+    group_by(customer_id) %>% 
+    summarise(
+        cust_name = unique(customer_name),
+        segment = unique(segment)
+        ) %>% 
+    rename(cust_id = customer_id)
+
 
 ## products table
-prod = raw[,.(cat = unique(Category),
-               subcat = unique(`Sub-Category`),
-               desc = unique(`Product Name`)),
-            by = "Product ID"]
-setnames(prod, "Product ID", "prod_ID")
+prod = 
+    raw %>% 
+    group_by(product_id) %>% 
+    summarise_at(
+        vars(category, sub_category, product_name),
+        unique
+        ) %>% 
+    rename(prod_id = product_id)
 
 ## locations table
-loc = raw[,.(city = unique(City),
-              state = unique(State),
-              region = unique(Region)),
-           by = "Postal Code"]
-
-setnames(loc, "Postal Code", "zip")
+loc = 
+    raw %>% 
+    group_by(postal_code) %>% 
+    summarise_at(
+        vars(city, state, region),
+        unique
+    ) %>% 
+    rename(zip = postal_code)
 
 ## ZIP duplicates handling
-loc$zip %>% unique() %>% length()
+loc %>% pull(zip) %>% unique() %>% length()
 nrow(loc)
 
-loc[duplicated(loc$zip),zip] %>% 
+loc %>% 
+    select(zip) %>% 
+    filter(duplicated(zip)) %>% 
     {filter(.data = loc, zip == .)}
 
-loc[zip == "92024" & city == "Encinitas", zip := 92028]
+loc =
+    loc %>% 
+    mutate(zip = 
+               ifelse(
+                   zip == "92024" & city == "Encinitas",
+                   92028,
+                   zip)
+           )
 
 ## transactions table
-pur = raw[,.(pur_ID = unique(`Order ID`),
-              date = unique(`Order Date`),
-              trans_mode = unique(`Ship Mode`),
-              cust_ID = unique(`Customer ID`),
-              prod_ID = unique(`Product ID`),
-              sales = Sales,
-              items = Quantity,
-              price = Sales/Quantity,
-              promo = Discount),
-           by = "Row ID"]
+pur = 
+    raw %>% 
+    group_by(row_id) %>% 
+    summarise(
+        pur_id = unique(order_id),
+        date = unique(order_date),
+        trans_mode = unique(ship_mode),
+        cust_id = unique(customer_id),
+        prod_id = unique(product_id),
+        sales = sales,
+        items = quantity,
+        price = sales / quantity,
+        promo = discount
+    )
 
 ## WHAT IS THAT SPLITTING GOOD FOR?
 ### relational databases logic...
 object.size(raw) %>% format(units = "Mb")
 
 (object.size(cust) + object.size(loc) + object.size(prod) +
-    object.size(pur)) %>% format(units = "Mb")
+        object.size(pur)) %>% format(units = "Mb")
 
 ### more realistic proportions
 (10 * object.size(raw)) %>% format(units = "Mb")
@@ -94,29 +120,29 @@ object.size(raw) %>% format(units = "Mb")
 rm(raw)
 
 #--- 1.2 EDA -------------------------------------------------------------------
-pur[,
-    promoC := ifelse(promo == 0, 0, 1)
-    ][,
-      .(cust_ID = unique(cust_ID),
-       date = unique(date),
-       sales = sum(sales),
-       items = sum(items),
-       price = mean(sales/items),
-       promoC = sum(promoC),
-       noSKU = .N),
-    by = "pur_ID"
-    ][,
-      promoC := promoC/noSKU
-      ] -> bas
+bas = 
+    pur %>% 
+    mutate(promoC = ifelse(promo == 0, 0, 1)) %>% 
+    group_by(pur_id) %>% 
+    summarise(
+        cust_id = unique(cust_id),
+        date = unique(date),
+        sales = sum(sales),
+        items = sum(items),
+        price = mean(sales/items),
+        promoC = sum(promoC),
+        noSKU = n()
+        ) %>% 
+    mutate(promoC := promoC/noSKU)
 
 ## sales histogram
 ggplot(bas, aes(x = sales)) +
     geom_histogram(binwidth = 1000)
 
-ggplot(bas[sales < 1000], aes(x = sales)) +
+ggplot(bas %>% filter(sales < 1000), aes(x = sales)) +
     geom_histogram(binwidth = 100)
 
-ggplot(bas[sales < 1000], aes(x = 1, y = sales)) +
+ggplot(bas %>% filter(sales < 1000), aes(x = 1, y = sales)) +
     geom_violin(draw_quantiles = c(0.25, 0.5, 0.75))
 
 ## the lower the prices, the more the items?
@@ -125,30 +151,31 @@ ggplot(bas, aes(x = items, y = price)) +
     geom_smooth(method = "loess")
 
 ## basket promo share
-ggplot(bas[order(bas$promoC),], aes(x = 1:nrow(bas), y = promoC)) +
+ggplot(bas %>% arrange(bas$promoC), aes(x = 1:nrow(bas), y = promoC)) +
     geom_line()
 
 ### analyse the periodicity on nopromo baskets ####
-np_bas =  bas[promoC == 0]                        #
-np_bas$cust_ID %>% unique %>% length              #
-                                                  #
-np_bas$cust_ID %>%                                #
-    table %>%                                     #
-    as.data.frame() %>%                           #
-    {.[order(.$Freq, decreasing = T),]} %>%       #
-    View()                                        #
-rm(np_bas)                                        #
-### nothing to analyse :D #########################
+np_bas =  bas %>% filter(promoC == 0)
+np_bas$cust_id %>% unique %>% length
+
+np_bas %>%
+    pull(cust_id) %>%
+    table %>%
+    as_tibble() %>%
+    arrange(desc(n)) %>%
+    View()
+rm(np_bas)                                        
+### nothing to analyse :( #########################
 
 ## basket unique SKU
-ggplot(bas[order(bas$noSKU),], aes(x = 1:nrow(bas), y = noSKU)) +
+ggplot(bas %>% arrange(noSKU), aes(x = 1:nrow(bas), y = noSKU)) +
     geom_line()
 
 ## relation between unique SKU and number of items bought
 ggplot(bas, aes(x = noSKU, y = items)) +
     geom_point() +
     geom_smooth(method = "lm")
-    ## BAD IDEA!
+## BAD IDEA!
 
 ggplot(bas, aes(x = as.factor(noSKU), y = items)) +
     geom_boxplot()
@@ -158,14 +185,14 @@ ggplot(bas, aes(x = noSKU, y = items)) +
     geom_smooth(method = "lm")
 
 ## daily visits
-bas[,
-    day := format(date, "%u")
-    ][,
-      promoG := ifelse(promoC > 0.5, 1, 0)
-    ][,
-      .(visits = .N),
-      by = .(day, promoG)
-    ] -> daily
+daily = 
+    bas %>% 
+    mutate(
+        day = format(date, "%u"),
+        promoG = ifelse(promoC > 0.5, 1, 0)
+        ) %>% 
+    group_by(day, promoG) %>% 
+    summarise(visits = n())
 
 ggplot(daily, aes(x = day, y = visits, col = as.factor(promoG),
                   group = as.factor(promoG))) +
@@ -176,38 +203,52 @@ ggplot(daily, aes(x = day, y = visits, col = as.factor(promoG),
 
 #--- 2.1 CUSTOMERS -------------------------------------------------------------
 ## feature engineering
-pur[,
-    .(unSKU = length(unique(prod_ID)),
-      itemsSKU = mean(items),
-      priceSKU = mean(price),
-      promoSKU = mean(promo)),
-    by = "cust_ID"] -> custStats
+custStats = 
+    pur %>% 
+    group_by(cust_id) %>% 
+    summarise(
+        unSKU = length(unique(prod_id)),
+        itemsSKU = mean(items),
+        priceSKU = mean(price),
+        promoSKU = mean(promo)
+    )
 
-bas[,
-    .(basSKU = mean(noSKU),
-      valueBas = mean(sales),
-      itemsBas = mean(items),
-      priceBas = mean(price)),
-    by = "cust_ID"] -> basStats
+basStats = 
+    bas %>% 
+    group_by(cust_id) %>% 
+    summarise(
+        basSKU = mean(noSKU),
+        valueBas = mean(sales),
+        itemBas = mean(items),
+        priceBas = mean(price)
+    )
 
-custStats[basStats, on = "cust_ID"
-          ][cust[,.(cust_ID, segment)],, on = "cust_ID"] -> custStats
+custStats = 
+    cust %>% 
+    select(cust_id, segment) %>% 
+    inner_join(custStats, by = "cust_id") %>% 
+    inner_join(basStats, by = "cust_id")
+
 rm(basStats)
 
 ## dummies
 custStats$segment %>% unique
 
-custStats %<>% 
-    mutate(Corp = ifelse(segment == "Corporate", 1, 0)) %>% 
-    mutate(HO = ifelse(segment == "Home Office", 1, 0)) %>% 
+custStats = 
+    custStats %>% 
+    mutate(corp = ifelse(segment == "Corporate", 1, 0)) %>% 
+    mutate(ho = ifelse(segment == "Home Office", 1, 0)) %>% 
     select(-segment)
 
 ## into matrix and scale
 custStats %>% summary()
-    
-custStatsS = custStats[,-1]
-custStatsS = apply(custStatsS, 2, scale)
-rownames(custStatsS) = custStats$cust_ID
+
+custStatsS = 
+    custStats %>% 
+    select(-cust_id) %>% 
+    scale()
+
+rownames(custStatsS) = custStats$cust_id
 
 ## kmeans
 set.seed(123)
@@ -221,34 +262,51 @@ k = 3
 
 custStats$group = kmeans(custStatsS, k)$cluster
 cluster::clusplot(custStatsS, custStats$group, color = TRUE, shade = T, labels = 1)
-table(custStats$group) 
+table(custStats$group)
 
 tree = rpart(as.factor(group)~., custStats[,-1])
 fancyRpartPlot(tree)
 
 #--- 2.1 PRODUCTS --------------------------------------------------------------
 ## feature engineering
-pur[,
-    .(custPen = length(unique(cust_ID))/nrow(cust),
-      basPen = length(unique(pur_ID))/nrow(bas),
-      items = mean(items),
-      price = mean(price),
-      promo = mean(promo)),
-    by = "prod_ID"] -> prodStats
+prodStats = 
+    pur %>% 
+    group_by(prod_id) %>% 
+    summarise(
+        custPen = length(unique(cust_id))/nrow(cust),
+        basPen = length(unique(pur_id))/nrow(bas),
+        items = mean(items),
+        price = mean(price),
+        promo = mean(promo)
+    )
 
 ## dummies
-prod$cat %>% table
-prodStats[prod[,.(prod_ID, cat)], on = "prod_ID"
-          ][,furn := ifelse(cat == "Furniture", 1, 0)
-            ][,tech := ifelse(cat == "Technology", 1, 0)
-              ][!duplicated(prod_ID), -"cat"] -> prodStats
+prodStats =
+    prod %>%
+    select(prod_id, category) %>% 
+    inner_join(prodStats, by = "prod_id")
 
+prod$category %>% table
+
+prodStats = 
+    prodStats %>% 
+    mutate(
+        furn = ifelse(category== "Furniture", 1, 0),
+        tech = ifelse(category == "Technology", 1, 0)
+    ) %>% 
+    select(-category) %>% 
+    filter(!duplicated(prod_id))
+    
 ## matrix and scale
-rownames(prodStats) = prodStats$prod_ID
-prodStatsS = apply(prodStats[,-1], 2, scale)
-rownames(prodStatsS) = prodStats$prod_ID
+prodStatsS = 
+    prodStats %>% 
+    ungroup() %>% 
+    select(-prod_id) %>% 
+    scale()
 
-cluster = hclust(dist(prodStatsS), method = "average")
+rownames(prodStatsS) = prodStats$prod_id
+
+cluster = hclust(dist(prodStatsS), method = "complete")
 # plot(cluster)
 
 set.seed(123)
@@ -286,16 +344,27 @@ tree = rpart(as.factor(group)~., prodStats[,-1])
 fancyRpartPlot(tree)
 
 ## which customers which items?
-custStats %<>% as.data.table()
-pur[custStats[,.(cust_ID, custG = group)], on = "cust_ID"
-    ][prodStats[,.(prod_ID, prodG = group)], on = "prod_ID"] -> purGrp
+purGrp = 
+    custStats %>% 
+    rename(custG = group) %>% 
+    select(cust_id, custG) %>% 
+    inner_join(pur, on = custid)
 
-purGrp[,.(custG, prodG)
-       ][,.(freq = .N),
-         by = .(custG, prodG)] -> purGrp
-purGrp[, sum := sum(freq), by = "custG"
-       ][,share := freq/sum
-         ][,.(custG, prodG, share)] -> purGrp
+purGrp = 
+    prodStats %>% 
+    rename(prodG = group) %>% 
+    select(prod_id, prodG) %>% 
+    inner_join(purGrp, on = custid)
+
+purGrp = 
+    purGrp %>% 
+    group_by(custG, prodG) %>% 
+    summarise(freq = n()) %>% 
+    ungroup() %>% 
+    group_by(custG) %>% 
+    mutate(sum = sum(freq)) %>% 
+    mutate(share = freq / sum) %>% 
+    select(custG, prodG, share)
 
 ggplot(purGrp, aes(x = custG, y = share , fill = as.factor(prodG))) +
     geom_col()
